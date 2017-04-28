cd597ffc5633a2cb2e5c1c30f5c603e6213bf162
Phoebe16 <1012580446@qq.com>
2017-04-28 23:43:59 +0800

    '组件路由的设置，1像素边框'

---
+++ src/components/seller/seller.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>seller</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style type="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+// import Hello from '@/components/Hello';
+import VueResource from 'vue-resource';
 
-Vue.use(Router)
+import Goods from '../components/goods/goods';
+import Ratings from '../components/ratings/ratings';
+import Seller from '../components/seller/seller';
+
+import '../common/stylus/index.styl';
+
+Vue.use(Router);
+Vue.use(VueResource);
 
 export default new Router({
+  // 配置当v-link元素匹配的路径时需要添加到元素上的class，默认为router-link-active
+  linkActiveClass: 'active',
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/goods',
+      name: 'Goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'Ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'Seller',
+      component: Seller
     }
   ]
-})
+});
