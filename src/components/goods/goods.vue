07173a1c17e8b3e77a6b549d2201b375db7ed238
Phoebe16 <1012580446@qq.com>
2017-05-01 10:16:51 +0800

    '运用better-scroll实现左右菜单对应联动'

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,8 @@
     <div class="goods">
         <div class="menu-wrapper" ref="menuWrapper">
             <ul>
-                <li v-for="item in goods" class="menu-item">
+                <li v-for="(item, index) in goods" class="menu-item"
+                :class="{'current': currentIndex === index}" @click="selectMenu(index)">
                     <span class="text border-1px">
                         <span v-show="item.type > 0" class="icon" :class="classMap[item.type]">
                         </span>{{item.name}}
@@ -12,7 +13,7 @@
         </div>
         <div class="foods-wrapper" ref="foodsWrapper">
             <ul>
-                <li v-for="item in goods" class="food-list">
+                <li v-for="item in goods" class="food-list food-list-hook">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
                         <li v-for="food in item.foods" class="food-item border-1px">
@@ -40,6 +41,7 @@
 
 <script type="text/ecmascript-6">
     import BScroll from 'better-scroll';
+
     const ERR_OK = 0;
 
     export default {
@@ -50,8 +52,22 @@
         },
         data() {
             return {
-                goods: []
+                goods: [],
+                listHeight: [],
+                scrollY: 0
             };
+        },
+        computed: {
+            currentIndex() {
+                for (let i = 0; i < this.listHeight.length; i++) {
+                    let height1 = this.listHeight[i];
+                    let height2 = this.listHeight[i + 1];
+                    if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                        return i;
+                    }
+                }
+                return 0;
+            }
         },
         created() {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -61,14 +77,39 @@
                     this.goods = response.data;
                     this.$nextTick(() => {
                         this._initScroll();
+                        this._calculateHeight();
                     });
                 }
             });
         },
         methods: {
+            selectMenu(index) {
+                let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+                let el = foodList[index];
+                this.foodsScroll.scrollToElement(el, 300);
+                console.log(index);
+            },
             _initScroll() {
-                this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
-                this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+                this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+                    // click: true
+                });
+                this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+                    probeType: 3
+                });
+
+                this.foodsScroll.on('scroll', (pos) => {
+                    this.scrollY = Math.abs(Math.round(pos.y));
+                });
+            },
+            _calculateHeight() {
+                let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+                let height = 0;
+                this.listHeight.push(height);
+                for (let i = 0; i < foodList.length; i++) {
+                    let item = foodList[i];
+                    height += item.clientHeight;  // 加上每个item的高度
+                    this.listHeight.push(height);
+                }
             }
         }
     };
@@ -94,6 +135,14 @@
                 width: 56px
                 padding: 0 12px
                 line-height: 14px
+                &.current
+                    position: relative
+                    z-index: 10
+                    margin-top: -1px
+                    background: #fff
+                    font-weight: 700
+                    .text
+                        border-none()
                 .icon
                     display: inline-block
                     vertical-align: top
